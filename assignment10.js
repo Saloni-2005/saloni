d9ae7030337f539fc99b7658207f61be0cca2975
Saloni Kumari <chaudharysaloni2510@gmail.com>
2024-10-07 09:35:22 +0530

    ass 8&10

---
+++ assignment10.js
@@ -0,0 +1,91 @@
+//    function createPerson(name,age){
+//      return{
+//         name,
+//         age
+//      }
+//    }
+//      function printPersonDetails(person){
+//         console.log(`name: ${person.name}`)
+//         console.log(`Age: ${person.age}`)
+//      }
+//        let person1=createPerson("Alice",30)
+//          printPersonDetails(person1)
+
+        //  function createPerson(name,age){
+        //     return{
+        //        name,
+        //        age
+        //     }
+        //   }
+        //    function printPersonInfo(person){
+        //      console.log(`name: ${person.name}`)
+        //              console.log(`Age: ${person.age}`)
+        //    }
+
+        //    let person2=createPerson("Sarah",35)
+        //       console.log(`Person 2:`)
+        //       printPersonInfo(person2)
+           
+
+
+        // function book(title,author){
+        //      this.title=title
+        //      this.author=author
+        // }
+        //   const book1=new book("The catcher in the Rye")
+        //    const book2=new book("1984","George Orwell")
+        //    console.log("book 1:")
+        //    console.log(book1)
+        //    console.log("book 2:")
+        //    console.log(book2)
+
+
+
+
+
+//    function createPerson(name,age){
+//        return {
+//            name,
+//            age
+//        }
+//    }
+//      function printPersonDetails(person){
+//         console.log(`name: ${person.name}`)
+//         console.log(`Age: ${person.age}`)
+//      }
+//       const person1= createPerson("Alice",30)
+//         printPersonDetails(person1)
+
+
+
+//   function createPerson(name,age){
+//     return{
+//         name,
+//         age
+//     }
+//   }
+//     function printPersonInfo(person){
+//         console.log(`name: ${person.name}`)
+//          console.log(`age: ${person.age}`)   
+        
+//     }
+      
+//  const person2=createPerson("Sarah",35)
+//    console.log("person 2:")
+//      printPersonInfo(person2)
+
+
+
+
+
+
+  // function book(title,author){
+  //   this.title=title
+  //   this.author=author
+  // }
+  //    const book1=new book("The catcher in the rye","J.DSalinger")
+  //    const book2=new book("1984", "George Orwell")
+  //     console.log("book 1:")
+  //     console.log(book1)
+  //     console.log("book 2 :")
+  //     console.log(book2)
---
+++ assignment8.js
@@ -0,0 +1,273 @@
+// Assignment: Conditional statements
+
+// Question 1: Grade Classification
+// Write a program that takes a student's grade (as a percentage) and uses a "switch" case statement to classify it into a letter grade (A, B, C, D, F). The grading scale is:
+
+// A: 90% and above
+
+// B: 80% to 89%
+
+// C: 70% to 79%
+
+// D: 60% to 69%
+
+// F: Below 60%
+
+// Display the letter grade to the user.
+
+
+//     let percentage = 98
+//     switch(true){
+//         case (percentage>=90):
+//             console.log("A")
+//                break;
+//         case (percentage>=80):
+//             console.log("B")
+//                break;  
+//         case (percentage>=70):
+//             console.log("C")
+//                break;
+//         case (percentage>=60):
+//             console.log("D")
+//                break;
+//         default:
+//             console.log("F")        
+//    }
+    
+
+
+
+// Question 2: Traffic Light Control
+// Create a program that simulates a traffic light system. The user inputs a color code (R for Red, Y for Yellow, G for Green), and the program uses a "switch" case statement to output the appropriate action:
+
+// Red: "Stop"
+
+// Yellow: "Get Ready"
+
+// Green: "Go"
+
+// For any other input, display "Invalid color code."
+
+  
+
+    //  let colour = "Y"
+    //    switch (true){
+    //     case (colour == "R"):
+    //         console.log("Stop")
+    //         break;
+    //     case (colour=="Y"):
+    //         console.log("Get Ready")
+    //         break;
+    //     case (colour=="G"):
+    //         console.log("Go")
+    //         break;
+    //       default:
+    //         console.log("Invalid color code")
+    //    }
+
+
+
+// Question 3: Age Group Classification
+// Create a program that classifies a person's age into different life stages. Ask the user to input their age and use if, else if, and else statements to classify and display:
+
+// "Child" for ages 0 to 12
+
+// "Teenager" for ages 13 to 19
+
+// "Adult" for ages 20 to 64
+
+// "Senior" for ages 65 and above
+
+// Display "Invalid age" for any negative age values.
+
+
+    // function age(a){
+    //      if (a>=0){
+    //         if(a<=12){
+    //             return "Child"
+    //         }
+    //         if(a<=19){
+    //             return "Teenager"
+    //         }
+    //         if(a<=64){
+    //             return "Adult"
+    //         }
+    //      }
+    //      else if(a>=65){
+    //         return "Senior"
+    //      }
+    //      else{
+    //         return "Invalid age"
+    //      }
+    // }
+    //   let classify=age(68)
+    //    console.log(classify)
+
+
+
+
+// Question 4: Shipping Calculator
+// Create a program that calculates shipping costs based on the shipping method selected. The user inputs a method code (S for Standard, E for Express, O for Overnight), and the program uses a "switch" case statement to display the cost:
+
+// Standard: Rs.5
+
+// Express: Rs.10
+
+// Overnight: Rs.20
+
+// For any other input, display "Invalid shipping method."
+
+
+
+//    let code="S"
+//      switch(true){
+//         case (code=="S"):
+//            console.log("Rs.5")
+//               break;
+//         case (code=="E"):
+//             console.log("Rs.10")
+//               break;
+//         case (code=="O"):
+//             console.log("Rs.20")
+//               break;
+//         default:
+//             console.log("Invalid shipping method")
+//      }
+    
+
+
+// Question 5: Subscription Plans
+// Write a program that determines the monthly cost of a subscription plan based on the user's choice. The options are:
+
+// 1. Basic Rs.9.99 per month
+
+// 2. Standard Rs.14.99 per month
+
+// 3. Premium Rs.19.99 per month
+
+// The user inputs a number corresponding to their choice, and the "switch" case statement displays the monthly cost. If the number is not between 1 and 3, display "Invalid plan."
+
+
+// let plan="Base"
+//   switch(true){
+//     case (plan=="Base"):
+//         console.log("Rs.9.99 per month")
+//            break;
+//     case (plan=="Standard"):
+//         console.log("Rs.14.99 per month")
+//            break;
+//     case (plan=="Premium"):
+//         console.log("Rs.19.99 per month")
+//            break;
+//     default:
+//         console.log("Invalid plan")
+//   }
+
+
+
+// let percentage=78
+//   switch(true){
+//     case(percentage>=90):
+//     console.log("A")
+//      break;
+//     case(percentage>=80):
+//     console.log("B")
+//     break;
+//     case(percentage>=70):
+//     console.log("C")
+//     break;
+//     case(percentage>=60):
+//     console.log("D")
+//     break;
+//     default:
+//         console.log("F")
+//   }
+
+
+
+//  let code="S"
+//    switch(true){
+//       case (code=="R"):
+//         console.log("Stop")
+//         break;
+//     case(code=="Y"):
+//       console.log("Get Ready")
+//       break;
+//     case(code=="G"):
+//      console.log("Go")
+//      break;
+//      default:
+//         console.log("Invalid colour code")
+//    }
+
+
+// function personsAge(a){
+//        if(a>=0){
+//           if(a<=12){
+//             return "Child"
+//           }
+//           if(a<=19){
+//             return "Teenager"
+//           }
+//           if(a<=64){
+//             return "Adult"
+//           }
+//     else if(a>=65){
+//         return "Senior"
+//     }
+//      else{
+//         return "Invalid Age"
+//      }}}
+//       let age = personsAge(56)
+//        console.log(age)
+
+
+
+
+
+
+//   let code="E"
+//    switch (true){
+//       case(code=="S"):
+//        console.log("Rs.5")
+//        break;
+//     case(code=="E"):
+//       console.log("Rs.10")
+//       break;
+//       case(code=="O"):
+//        console.log("Rs.20")
+//        break;
+//     default:
+//         console.log("Invalid shipping method")
+//    }
+
+
+
+
+
+
+
+// let plan="Standard"
+//  switch(true){
+//     case(plan=="Basic"):
+//       console.log("Rs.9.99 per month")
+//       break;
+//     case(plan=="Standard"):
+//       console.log("Rs.14.99 per month")
+//       break;
+//     case (plan=="Premium"):
+//         console.log("Rs.19.99 per month")
+//       break;
+//     default:
+//         console.log("Invalid plan")
+//  }
+
+
+
+
+
+
+
+
+
+
